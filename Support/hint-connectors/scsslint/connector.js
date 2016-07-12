6f1ee9bcb7e8c7cf1341cfcbe0a8de1de1445a45
Graham Heath <heathg@google.com>
2016-07-12 16:19:33 -0700

    use tabs (groan)

--- Support/hint-connectors/scsslint/connector.js
+++ Support/hint-connectors/scsslint/connector.js
@@ -4,43 +4,43 @@
  * parses the output into a JS object.
  */
 var path = require('path'),
-  getJsonOutput = require('../helpers/getjsonoutput'),
-  Q = require('q');
+	getJsonOutput = require('../helpers/getjsonoutput'),
+	Q = require('q');
 
 module.exports = {
-  /**
-   * Process the file using scss-lint
-   * @param {Array} files Array of files to check with the specified linter
-   * @returns {Q.Promise} Returns a promise that is resolved when the output is
-   * parsed to a JS object.
-   */
-  process: function (files) {
-    var def = Q.defer();
-    var fileDir = path.dirname(files[0]);
-    var args = ['--format', 'JSON'].concat(files);
-    var originalOutput = getJsonOutput('scss-lint', args, {cwd: fileDir});
+	/**
+	 * Process the file using scss-lint
+	 * @param {Array} files Array of files to check with the specified linter
+	 * @returns {Q.Promise} Returns a promise that is resolved when the output is
+	 * parsed to a JS object.
+	 */
+	process: function (files) {
+		var def = Q.defer();
+		var fileDir = path.dirname(files[0]);
+		var args = ['--format', 'JSON'].concat(files);
+		var originalOutput = getJsonOutput('scss-lint', args, {cwd: fileDir});
 
-    /**
-     * Original JSON output needs to be transformed so it can be used with the
-     * default renderer & tooltip.
-     */
-    originalOutput
-      .then(function (output) {
-        var errors = (output[files[0]]||[]).map(function (error) {
-          return {
-            hinttype: 'scss',
-            file: files[0],
-            line: error.line,
-            column: error.column,
-            evidence: error.evidence || '',
-            message: error.reason +
-                (error.linter ? ' (' + error.linter + ')' : ''),
-            rule: error.code
-          };
-        });
-        def.resolve(errors);
-      }, def.reject)
-      .done();
-    return def.promise;
-  }
+		/**
+		 * Original JSON output needs to be transformed so it can be used with the
+		 * default renderer & tooltip.
+		 */
+		originalOutput
+			.then(function (output) {
+				var errors = (output[files[0]]||[]).map(function (error) {
+					return {
+						hinttype: 'scss',
+						file: files[0],
+						line: error.line,
+						column: error.column,
+						evidence: error.evidence || '',
+						message: error.reason +
+								(error.linter ? ' (' + error.linter + ')' : ''),
+						rule: error.code
+					};
+				});
+				def.resolve(errors);
+			}, def.reject)
+			.done();
+		return def.promise;
+	}
 };
